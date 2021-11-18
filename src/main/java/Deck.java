ce95603a92c928d6d80181cddb2dcd624c039d2e
Selasi Ayittah <selasiayittah3@gmail.com>
2021-11-18 16:58:05 +0000

    my first commit on the blackjack project

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Deck {
 
     private ArrayList<Cards> cards;
+    List<Cards> userOneCard;
 
     public Deck() {
         this.cards = new ArrayList<Cards>();
@@ -13,12 +13,25 @@ public class Deck {
         return cards;
     }
 
-    public List<Cards> fullDeck(){
+
+    public ArrayList<Cards> fullDeck(){
         for (CardSuits cardSuits : CardSuits.values()){
             for (CardValue cardValue :  CardValue.values()){
                 this.cards.add(new Cards(cardSuits,cardValue.getValue()));
             }
+            shuffleCard();
         } return cards;
+    }
+
+
+    public void shuffleCard(){
+       Collections.shuffle(cards);
+    }
+
+
+    public void getOneCard(){
+         cards.remove(0);
+
     }
 
 
